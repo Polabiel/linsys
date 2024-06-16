f66d08e0a1b2d8b2a13b19d8d552012a418451b8
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2024-06-16 15:39:25 -0300

    chore: Update generate_markdown function to use os.path for file paths

--- src/export.py
+++ src/export.py
@@ -1,17 +1,22 @@
 import os
 
-def generateMarkdown(strings) -> None:
-    # Verifica se a pasta "instances" existe, caso contrário, cria-a
-    if not os.path.exists("instances"):
-        os.makedirs("instances")
+def generate_markdown(strings: str, title: str) -> list[str]:
+    # Obtém o diretório atual
+    current_dir: str = os.path.dirname(os.path.abspath(__file__))
 
-    # Gera o nome do arquivo Markdown baseado na quantidade de strings
-    nome_arquivo: str = f"instances/markdown_{len(strings)}.md"
+    # Define o caminho para a pasta "instances" fora do diretório "src"
+    instances_dir: str = os.path.join(current_dir, '..','instances')
+
+    # Verifica se a pasta "instances" existe, caso contrário, cria-a
+    if not os.path.exists(instances_dir):
+        os.makedirs(instances_dir)
+
+    # Gera o nome do arquivo Markdown baseado no título
+    nome_arquivo: str = os.path.join(instances_dir, f"markdown_{title.replace(' ', '_')}.md")
 
     # Abre o arquivo em modo de escrita
-    with open(nome_arquivo, "w") as arquivo:
-        # Escreve as strings no arquivo Markdown
-        for string in strings:
-            arquivo.write(f"- {string}\n")
-
+    with open(nome_arquivo, "w", encoding='utf-8') as arquivo:
+        # Escreve o conteúdo de strings no arquivo
+        arquivo.write(strings)
+       
     print(f"Arquivo Markdown gerado em {nome_arquivo}")
