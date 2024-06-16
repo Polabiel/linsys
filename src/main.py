e6d07b8283219a470954e723eddf8ae0d6967f8b
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2024-06-16 15:39:13 -0300

    feat: Add linear programming solver for problem 4
    The commit adds a linear programming solver for problem 4. It uses the linprog function from the scipy.optimize library to find the optimal solution. The problem statement and constraints are defined in the code.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from scipy.optimize import minimize, OptimizeResult
 import numpy as np
 from colorama import Fore, Style
 
+from export import generate_markdown
+
 def problem_1():
     """
     minimizar: 5x_1 + x_2
@@ -93,23 +95,27 @@ def problem_4():
     x_j ≥ 0 para j = 1, 2, 3
     A solução ótima deste problema é x^* = (400, 0, 0, 0) com f(x^*) = 0.
     """ 
+    
     # Coeficientes da função objetivo
     c: list[int] = [0, 0, 10, 10]
-
-    # Matriz de coeficientes das desigualdades
-    A: list[list[int]] = [[1, 1, 1, 1], [1, -2, 0, 0], [0, 1, -2, 0], [0, 0, 1, -2]]
-
-    # Vetor de termos independentes das desigualdades
-    b: list[int] = [400, 0, 0, 0]
-
-    # Limites para as variáveis
-    x0_bounds: tuple[Literal[0], None] = (0, None)
-    x1_bounds: tuple[Literal[0], None] = (0, None)
-    x2_bounds: tuple[Literal[0], None] = (0, None)
-    x3_bounds: tuple[Literal[0], None] = (0, None)
-
-    res: OptimizeResult = linprog(c, A_eq=A, b_eq=b, bounds=[x0_bounds, x1_bounds, x2_bounds, x3_bounds], method='highs')
     
+    A: list[list[int]] = [[-1, 2, 0, 0],[0, -1, 2, 0],[0, 0, -1, 2]]
+
+    # Vetor b representando os limites superiores das desigualdades
+    b: list[int] = [0, 0, 0]
+
+    # Coeficientes para a equação de igualdade
+    A_eq: list[list[int]] = [[1, 1, 1, 1]]
+
+    # Vetor b_eq representando o limite da equação de igualdade
+    b_eq: list[int] = [400]
+
+    # Limites para cada variável
+    x_bounds: list[tuple[Literal[0], None]] = [(0, None), (0, None), (0, None), (0, None)]
+
+    # Resolvendo o problema de otimização
+    res: OptimizeResult = linprog(c, A_ub=A, b_ub=b, A_eq=A_eq, b_eq=b_eq, bounds=x_bounds, method='highs')
+
     print_result(res, "Problema 4", "A solução ótima deste problema é x^* = (400, 0, 0, 0) com f(x^*) = 0")
 
 def problem_5():
@@ -174,22 +180,36 @@ def problem_7():
     print_result(res, "Problema 7", "A solução ótima deste problema é x^* = (0, 1, 11) com f(x^*) = 36") # Mas não existe uma solução para este problema (porque não foi especificado no enunciado)
     
 def print_result(res, problem_title: str, solution_greet: str = "Solução ótima"):
+
+    result_str: str = ''
     
     if(res == None):
         print(Fore.RED + f"O problema {problem_title} não foi resolvido pois não foi especificado no enunciado" + Style.RESET_ALL)
         print(Fore.GREEN + "-------------------------------------" + Style.RESET_ALL)
+        result_str += "# O problema {problem_title} não foi resolvido pois não foi especificado no enunciado"
     else:
         print(Fore.WHITE + f"{problem_title}" + Style.RESET_ALL)
+        result_str += f"# {problem_title}\n"
         print(Fore.GREEN + "Resultado: " + Style.RESET_ALL)
+        result_str += "## Resultado:\n"
         print(Fore.BLUE + f"x1 = {res.x[0]}" + Style.RESET_ALL)
+        result_str += f"*X1* = `{res.x[0]}`\n"
         print(Fore.BLUE + f"x2 = {res.x[1]}" + Style.RESET_ALL)
+        result_str += f"*X2* = `{res.x[1]}`\n"
         print(Fore.YELLOW + f"Função objetivo = {res.fun}" + Style.RESET_ALL)
+        result_str += f"### Função objetivo = `{res.fun}`\n"
         print(Fore.CYAN + f"Status = {res.status}" + Style.RESET_ALL)
         print(Fore.MAGENTA + f"Mensagem = {res.message}" + Style.RESET_ALL)
+        result_str += f"Mensagem = {res.message}\n"
         print(Fore.RED + f"Iterações = {res.nit}" + Style.RESET_ALL)
+        result_str += f"Iterações = {res.nit}\n"
         print(Fore.YELLOW + f"{solution_greet}" + Style.RESET_ALL)
+        result_str += f"{solution_greet}\n"
         print(Fore.LIGHTCYAN_EX + "Solução Adquirida = " + str(res.x) + Style.RESET_ALL)
+        result_str += f"Solução Adquirida = {str(res.x)}\n"
         print(Fore.GREEN + "-------------------------------------" + Style.RESET_ALL)
+        
+    generate_markdown(result_str, problem_title)
         
 if __name__ == "__main__":
     problem_1()
